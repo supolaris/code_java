d30980bb7b8e3faaa995c27d8167d1ee5c7bf28b
Suleman1413 <msulaman061@gmail.com>
2022-10-27 11:22:31 +0500

    Basic syntax
    How the code should be written

---
+++ Code.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import javax.swing.JOptionPane;
+public class Code{
+    public static void main(String[] args) {
+        try (Scanner keyboardInput = new Scanner(System.in)){
+            System.out.println("The java program");        
+        }
+    }
+}
