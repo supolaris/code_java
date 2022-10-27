c81ca673bb339e567ef59299249d043ec917e24f
Suleman1413 <msulaman061@gmail.com>
2022-10-27 11:40:13 +0500

    Guess the word
    We will enter out guess word if correct then win or else you loose or out of guess.

--- Code.java
+++ Code.java
@@ -3,7 +3,31 @@ import javax.swing.JOptionPane;
 public class Code{
     public static void main(String[] args) {
         try (Scanner keyboardInput = new Scanner(System.in)){
-            System.out.println("The java program");        
-        }
+            System.out.println("The java program \t Guessing game");
+            String myWord = "supolaris";
+            String guessWord = "";
+            int guessCount  = 0;
+            int guessLimit = 3;
+            boolean outofGuess = false;
+
+            while (!myWord.equals(guessWord) && !outofGuess){
+                if (guessCount < guessLimit){
+                    System.out.print("Enter the guess Word: ");
+                    guessWord = keyboardInput.nextLine();
+                    guessCount++ ;
+                }
+                else{
+                    outofGuess = true;   
+                }  
+
+            }
+            if (outofGuess){
+                System.out.println("You are out of guess!");
+            }
+            else{
+                System.out.println("You win");   
+            }
+            
     }
+}
 }
