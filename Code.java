92c36838c5be1f2b53c0d0b849ba167cd3d923ae
Suleman1413 <msulaman061@gmail.com>
2022-10-28 09:12:11 +0500

    constructor code
    Constructors are the update and precise method of objects in classes.

--- Code.java
+++ Code.java
@@ -1,9 +1,25 @@
 import java.util.*;
 import javax.swing.JOptionPane;
+
+class Student {
+    String sName;
+    int sId;
+    String sDepartment;
+
+    public Student(String ssName, int ssId, String ssDepartment){
+        this.sName = ssName;
+        this.sId =  ssId;
+        this.sDepartment = ssDepartment;
+    }
+}
+
 public class Code{
     public static void main(String[] args) {
         try (Scanner keyboardInput = new Scanner(System.in)){
-            System.out.println("The java program");        
+            System.out.println("The java program \t Constructor method");
+            
+            Student studen1 = new Student("Suleman", 627, "UIIT");
+            System.out.println(studen1.sName + " " + studen1.sId );
         }
     }
 }
