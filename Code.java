7e72a25c35b5d314409d8886f3be662997e40731
Suleman1413 <msulaman061@gmail.com>
2022-10-28 08:39:56 +0500

    new class added
    In the same file new class is added. while first one was from other file.

--- Code.java
+++ Code.java
@@ -1,9 +1,30 @@
 import java.util.*;
 import javax.swing.JOptionPane;
+
+class Department{
+    int departId;
+    String departName;
+    String departLoc;
+}
+
 public class Code{
     public static void main(String[] args) {
         try (Scanner keyboardInput = new Scanner(System.in)){
-            System.out.println("The java program");        
+            System.out.println("The java program \t Class and Object");
+
+            
+            // class from other file
+            Student student1 = new Student();
+            student1.fName = "Muhammad";
+            student1.lName = "Suleman";
+            System.out.println(student1.fName + " " + student1.lName);
+
+
+            // class inside same file
+            Department depart1 = new Department();
+            depart1.departId = 006;
+            depart1.departName = "UIIT";
+            System.out.println(depart1.departId + "\t" + depart1.departName);
         }
     }
 }
---
+++ Student.java
@@ -0,0 +1,7 @@
+public class Student {
+    String fName;
+    String lName;
+    int rollNo;
+    String major;
+    Double cgpa;
+}
