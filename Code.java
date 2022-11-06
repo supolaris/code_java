70b6c3ac28981846ca37123dd74301aa28df4287
Suleman1413 <msulaman061@gmail.com>
2022-11-06 12:48:34 +0500

    Update code java

--- Code.java
+++ Code.java
@@ -1,36 +1,34 @@
-import java.util.Scanner;
+import java.util.*;
+import javax.swing.JOptionPane;
 public class Code{
     public static void main(String[] args) {
-        try(Scanner keyboardInput = new Scanner(System.in)){
-            String myWord = "Star";
+        try (Scanner keyboardInput = new Scanner(System.in)){
+            System.out.println("The java program \t Guessing game");
+            String myWord = "supolaris";
             String guessWord = "";
+            int guessCount  = 0;
             int guessLimit = 3;
-            int guessCount = 0;
+            int points = 10;
             boolean outofGuess = false;
 
-
-            while (!guessWord.equals(myWord) && !outofGuess){
-                if(guessCount < guessLimit){
-                    System.out.print("Enter word: ");
+            while (!myWord.equals(guessWord) && !outofGuess){
+                if (guessCount < guessLimit){
+                    System.out.print("Enter the guess Word: ");
                     guessWord = keyboardInput.nextLine();
-                    guessCount ++ ;
+                    guessCount++ ;
                 }
                 else{
-                    outofGuess = true;
-                }
+                    outofGuess = true;   
+                }  
+
             }
-            if(outofGuess){
-                System.out.print("You are out of guess");
+            if (outofGuess){
+                System.out.println("You are out of guess!");
             }
             else{
-                System.out.print("You win");
+                JOptionPane.showMessageDialog(null, "You win and got " + points + " points :)");
             }
-
-
             
-            
-
-
-        }
     }
+}
 }
