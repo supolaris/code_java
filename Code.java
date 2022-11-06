8e5b3f2863b960b497da8961c37e3d48dbdfaae9
Suleman1413 <msulaman061@gmail.com>
2022-11-06 13:00:22 +0500

    Update code.jana
    Boolean condition added

--- Code.java
+++ Code.java
@@ -5,11 +5,22 @@ class Student {
     String sName;
     int sId;
     String sDepartment;
+    double scgpa;
 
-    public Student(String ssName, int ssId, String ssDepartment){
+    public Student(String ssName, int ssId, String ssDepartment, double sscgpa){
         this.sName = ssName;
         this.sId =  ssId;
         this.sDepartment = ssDepartment;
+        this.scgpa = sscgpa;
+    }
+
+    public boolean isHonorable () {
+        if (this.scgpa > 3.5){
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 }
 
@@ -18,8 +29,9 @@ public class Code{
         try (Scanner keyboardInput = new Scanner(System.in)){
             System.out.println("The java program \t Constructor method");
             
-            Student studen1 = new Student("Suleman", 627, "UIIT");
+            Student studen1 = new Student("Suleman", 627, "UIIT", 3.6);
             System.out.println(studen1.sName + " " + studen1.sId );
+            System.out.println( "The student cgpa is greater then 3.5 " + studen1.isHonorable());
         }
     }
 }
