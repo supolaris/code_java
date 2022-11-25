cecd4992e40442c39faf5f5318b2ed100248f217
Muhammad Suleman <88381507+supolaris@users.noreply.github.com>
2022-11-25 16:07:30 +0500

    update code.java
    all okay with setter and getter.

--- Code.java
+++ Code.java
@@ -2,10 +2,10 @@ import java.util.Scanner;
 
     //class
         class Student{
-        String sName;
-        int sRoll;
-        Double sGpa;
-        String sDepartment;
+        private String sName;
+        private int sRoll;
+        private Double sGpa;
+       private String sDepartment;
     
     //constructor
     public Student(String ssName, int ssRoll, Double ssGpa, String ssDepartment){
@@ -25,7 +25,7 @@ import java.util.Scanner;
         }
     }
 
-    //setter
+    //setter for department variable
     public void setsDepartment(String setterDepart){
         if (setterDepart.equals("UIIT") || setterDepart.equals("Business")){
             this.sDepartment = setterDepart;
@@ -33,7 +33,36 @@ import java.util.Scanner;
         else{
             this.sDepartment = "unknown";
         }
-    }    
+    }  
+    public String getsDepartment(){
+        return sDepartment;
+    }  
+
+    //setter for name variable
+    public void setsName(String ssName){
+        this.sName = ssName;
+    }
+    public String getName(){
+        return sName;
+    }
+
+    //setter for rollno variable
+    public void setRoll(int ssroll){
+        this.sRoll = ssroll;
+    }
+    public int getRoll(){
+        return sRoll;
+    }
+
+    //setter for gpa
+    public void setGpa(Double ssgpa){
+        this.sGpa = ssgpa;
+    }
+    public Double getGpa(){
+        return sGpa;
+    }
+
+
 
     }
 
@@ -46,15 +75,15 @@ public class Code{
             Student student2 = new Student("James", 007, 3.0, "Business");
 
 
-            student1.setsDepartment("null");
+            student1.setsDepartment("new department");
 
             
 
             System.out.println("INFORMATION OF STUDENT");
             System.out.println("==========================");
-            System.out.println("Name : " + student1.sName + " GPA: " + student1.sGpa + " Roll number: " + student1.sRoll + " Department: " + student1.sDepartment);
+            System.out.println("Name : " + student1.getName() + " GPA: " + student1.getGpa() + " Roll number: " + student1.getRoll() + " Department: " + student1.getsDepartment());
             System.out.println("Is cgpa is greater than 3.3: " + student1.isMerit());
-            System.out.println("Name : " + student2.sName + " GPA: " + student2.sGpa + " Roll number: " + student2.sRoll);
+            System.out.println("Name : " + student2.getName() + " GPA: " + student2.getGpa() + " Roll number: " + student2.getRoll());
             System.out.println("Is cgpa is greater than 3.3: " + student2.isMerit());
  
 
