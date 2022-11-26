8b93323fc5695f64c52b235924b837a8e505d93a
Muhammad Suleman <88381507+supolaris@users.noreply.github.com>
2022-11-26 16:21:47 +0500

    update

--- Code.java
+++ Code.java
@@ -1,9 +1,55 @@
-import java.util.*;
-import javax.swing.JOptionPane;
+import java.util.Scanner;;
+class Student{
+    private String sName;
+    private String sDepartment;
+    private int sRoll;
+
+
+
+    public Student(String sName, String sDepartment, int sRoll){
+        this.sName = sName;
+        this.sDepartment = sDepartment;
+        this.sRoll = sRoll;
+    }
+
+
+
+    public void setsName(String ssName){
+        this.sName = ssName;
+    }
+    public String getsName(){
+        return sName;
+    }
+
+    public void setsDepartment(String ssDepartment){
+        this.sDepartment = ssDepartment;
+    }
+    public String getsDepartment(){
+        return sDepartment;
+    }
+
+    public void setsRoll(int ssRoll){
+        this.sRoll = ssRoll;
+    }
+    public int getsRoll(){
+        return sRoll;
+    }
+
+
+
+}
+
 public class Code{
     public static void main(String[] args) {
         try (Scanner keyboardInput = new Scanner(System.in)){
-            System.out.println("The java program");        
+            // String input = keyboardInput.nextLine();
+             
+            Student student1 = new Student("james", "movie", 007);
+            System.out.print("Enter user name: ");
+            student1.setsName(keyboardInput.nextLine());
+            // student1.sName = keyboardInput.nextLine();
+            System.out.println("The user name is: " + student1.getsName());
         }
+        
     }
 }
