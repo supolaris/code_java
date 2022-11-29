f9be80ae77aa6ebad753f5b19ce496a6bbaa91b4
Suleman1413 <msulaman061@gmail.com>
2022-11-29 11:00:06 +0500

    update code.java

--- Code.java
+++ Code.java
@@ -1,9 +1,20 @@
 import java.util.*;
+import java.io.File;
+import java.io.IOException;
 import javax.swing.JOptionPane;
 public class Code{
     public static void main(String[] args) {
         try (Scanner keyboardInput = new Scanner(System.in)){
-            System.out.println("The java program");        
+            System.out.println("The java program");      
+            File myFile = new File("C:dataFile.txt");
+            Scanner myReader = new Scanner(myFile);
+            while (myReader.hasNextLine()){
+                String data = myReader.nextLine();
+                System.out.println(data);
+            }
+            myReader.close();
+        }catch(Exception e){
+            System.out.println("An error occured");
         }
     }
 }
