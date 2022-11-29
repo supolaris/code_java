ef42066dc2804576cb1540ea640313f33dad33b8
Suleman1413 <msulaman061@gmail.com>
2022-11-29 10:28:54 +0500

    update code.java
    File handling file created.

--- Code.java
+++ Code.java
@@ -1,9 +1,19 @@
 import java.util.*;
+import java.io.File;
+import java.io.IOException;
 import javax.swing.JOptionPane;
 public class Code{
     public static void main(String[] args) {
         try (Scanner keyboardInput = new Scanner(System.in)){
-            System.out.println("The java program");        
+            File myFile = new File("C:dataFile.txt");
+            if (myFile.createNewFile()){
+                System.out.println("File name: " + myFile.getName());
+            }
+            else{
+                System.out.println("File already exists");
+            }
+        } catch (Exception e) {
+            System.out.println("There is an error");
         }
     }
 }
